c5e5230b9677f4cf85e10824c9b6b844d693f6da
Javahir Majidli <cevahirvelizade@gmail.com>
2022-12-08 16:48:34 +0100

    updated tc1

--- src/test_cases/TC1.java
+++ src/test_cases/TC1.java
@@ -3,5 +3,7 @@ package test_cases;
 public class TC1 {
     public static void main(String[] args) {
         System.out.println("practise again");
+
+        System.out.println("more times");
     }
 }
